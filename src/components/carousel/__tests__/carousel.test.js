02e600578866a9b642c8c50ef78424e12d5fac40
eshekak <olenev.biz@gmail.com>
2020-09-06 18:23:43 +0300

    feat/markup: Carousel tests scenarios

--- src/components/carousel/__tests__/carousel.test.js
+++ src/components/carousel/__tests__/carousel.test.js
@@ -11,4 +11,24 @@ describe("<Carousel />", () => {
                 .toJSON()).toMatchSnapshot();
         });
     });
+
+    describe("scrolling", () => {
+        // 0. Probably, have to pass into pocketsInfo
+
+        // 0*. Probably, has to check pocketsInfo for valid fields
+
+        // 1. Set currentPocketIdx = 1 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
+        // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
+
+        // 2. Set currentPocketIdx = 1 -> endX = 2 -> startX = 3, check that carouselElement.current.children[currentPocketIdx - 1].scrollIntoView
+        // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
+
+        // 3. Set currentPocketIdx = 2 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
+        // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
+
+        // 4. Set currentPocketIdx = 0 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
+        // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
+
+        expect(1).toEqual(1);
+    });
 });
