ba000e704882a14df8d9678cfb1e6f9eeaff9051
eshekak <olenev.biz@gmail.com>
2020-09-07 10:30:47 +0300

    feat/connect-to-store: Created carousel factory

--- src/components/carousel/carousel-factory.js
+++ src/components/carousel/carousel-factory.js
@@ -1,26 +1,35 @@
 import React from "react";
 import constant from "lodash/constant.js";
+import get from "lodash/get.js";
+import map from "lodash/map.js";
 
+import store from "../../store/store.js";
+import currencies from "../../lib/consts/currencies/currencies.js";
 import Input from "../input/input.js";
 
 import Carousel from "./carousel.js";
 
-const fromPockets = [{
-    currency: "USD",
-    input: constant(<Input prefix={constant("-")} value={145.67} />),
-    balance: "You have 58.33$"
-}, {
-    currency: "EUR",
-    input: constant(<Input prefix={constant("-")} value={145.67} />),
-    balance: "You have 58.33$"
-}, {
-    currency: "GBP",
-    input: constant(<Input prefix={constant("-")} value={145.67} />),
-    balance: "You have 58.33$"
-}];
+function createPocketsSpecification() {
+    const wallets = get(store, "wallets", {});
+
+    return map(wallets, (balance, currency) => ({
+        currency,
+        input: constant(<Input prefix={constant("-")} value={balance}/>),
+        balance: walletBalance(currency)
+    }));
+}
+
+function walletBalance(currency) {
+    const balanceNumber = get(store, `wallets[${currency}]`, "");
+    const currencySymbol = get(currencies, `${currency}.symbol`, "");
+
+    return `You have ${currencySymbol}${balanceNumber}`;
+}
 
 function CarouselFactory() {
-    return <Carousel pockets={fromPockets} />;
+    const pockets = createPocketsSpecification();
+
+    return <Carousel pockets={pockets} />;
 }
 
 export default CarouselFactory;
