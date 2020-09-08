aeb8b53a66790f503157c274b731e2632bb3397c
eshekak <olenev.biz@gmail.com>
2020-09-09 01:01:04 +0300

    feat/connect-to-net: Fixed input bug for chrome x2

--- src/components/carousel/carousel-input.js
+++ src/components/carousel/carousel-input.js
@@ -6,10 +6,11 @@ import Input from "../input/input.js";
 import {dispatch} from "../../lib/state-management/registry.js";
 import MUTATE_STORE from "../../events/mutate-store.js";
 
-// eslint-disable-next-line sonarjs/cognitive-complexity,max-statements
+// eslint-disable-next-line sonarjs/cognitive-complexity,complexity,max-statements
 function defineEventPayload(balance, inputEvent, value) {
     const newValue = inputEvent.target.value;
 
+    // eslint-disable-next-line no-negated-condition
     if (!window.chrome) {
         if (newValue === "") {
         // eslint-disable-next-line fp/no-mutation,no-param-reassign
@@ -21,14 +22,16 @@ function defineEventPayload(balance, inputEvent, value) {
         if (((/^\d+\.\d{0,2}$/u).test(newValue) || (/^\d+$/u).test(newValue)) && Number(newValue) <= Number(balance)) {
             return [["exchangeAmount"], newValue];
         }
-    } else if (newValue === "" && (/^\d+\.\d{0,2}$/u).test(value)) {
-        // eslint-disable-next-line fp/no-mutation,no-param-reassign
-        inputEvent.target.value = "";
-    }
+    } else {
+        if ((/^\d+\.\d{2}$/u).test(value) && newValue === "") {
+            // eslint-disable-next-line fp/no-mutation,no-param-reassign
+            inputEvent.target.value = "";
+        }
 
-    // eslint-disable-next-line prefer-named-capture-group,security/detect-unsafe-regex,unicorn/no-unsafe-regex
-    if ((/^\d+(\.\d{0,2})?$/u).test(newValue) && Number(newValue) <= Number(balance)) {
-        return [["exchangeAmount"], newValue];
+        // eslint-disable-next-line prefer-named-capture-group,security/detect-unsafe-regex,unicorn/no-unsafe-regex
+        if ((newValue === "" || (/^\d+(\.\d{0,2})?$/u).test(newValue)) && Number(newValue) <= Number(balance)) {
+            return [["exchangeAmount"], newValue];
+        }
     }
 
     return [["exchangeAmount"], value];
