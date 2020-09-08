70e46922a37dbce7f11bd4e11c55b8e10c6e6185
eshekak <olenev.biz@gmail.com>
2020-09-08 10:31:04 +0300

    feat/connect-to-store: Fixed tests for the carousel

--- src/components/carousel/__tests__/carousel.test.js
+++ src/components/carousel/__tests__/carousel.test.js
@@ -4,6 +4,7 @@ import constant from "lodash/constant.js";
 
 import Carousel from "../carousel.js";
 import Input from "../../input/input.js";
+import * as scrollToPocket from "../scroll-to-pocket.js";
 
 describe("<Carousel />", () => {
     const pockets = [{
@@ -34,50 +35,23 @@ describe("<Carousel />", () => {
         });
     });
 
-    describe.skip("scrolling", () => {
+    describe("scrolling", () => {
         it("scrolls to the right", () => {
-            const currentPocketIdx = 1;
             const startX = 1;
             const endX = 1;
-            const scrollIntoView = jest.fn();
+
+            scrollToPocket.default = jest.fn();
 
             React.useState = jest
                 .fn()
                 .mockReturnValue("default")
-                .mockReturnValueOnce([currentPocketIdx, jest.fn(() => currentPocketIdx + 1)])
                 .mockReturnValueOnce([startX, jest.fn()])
                 .mockReturnValueOnce([endX, jest.fn()]);
 
-            React.useRef = jest.fn().mockReturnValue({current: {children: [{}, {}, {scrollIntoView}]}});
-
             renderer
-                .create(<Carousel pockets={pockets} />);
+                .create(<Carousel activeCurrency={pockets[0].currency} pockets={pockets} />);
 
-            expect(scrollIntoView).toHaveBeenCalledWith({block: "start", inline: "nearest", behavior: "smooth"});
+            expect(scrollToPocket.default).toHaveBeenCalled();
         });
-
-        // 0. Probably, have to pass into pocketsInfo
-
-        // 0*. Probably, has to check pocketsInfo for valid fields
-
-        // eslint-disable-next-line max-len
-        // 1. Set currentPocketIdx = 1 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-        // eslint-disable-next-line max-len
-        // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-        // eslint-disable-next-line max-len
-        // 2. Set currentPocketIdx = 1 -> endX = 2 -> startX = 3, check that carouselElement.current.children[currentPocketIdx - 1].scrollIntoView
-        // eslint-disable-next-line max-len
-        // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-        // eslint-disable-next-line max-len
-        // 3. Set currentPocketIdx = 2 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-        // eslint-disable-next-line max-len
-        // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-        // eslint-disable-next-line max-len
-        // 4. Set currentPocketIdx = 0 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-        // eslint-disable-next-line max-len
-        // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
     });
 });
--- src/components/carousel/__tests__/scroll-to-pocket.test.js
+++ src/components/carousel/__tests__/scroll-to-pocket.test.js
@@ -15,28 +15,4 @@ describe("Scroll to pocket", () => {
         // eslint-disable-next-line max-len
         expect(testCarouselElement.current.children[0].scrollIntoView).toHaveBeenCalledWith({block: "start", inline: "nearest", behavior: "smooth"});
     });
-
-    // 0. Probably, have to pass into pocketsInfo
-
-    // 0*. Probably, has to check pocketsInfo for valid fields
-
-    // eslint-disable-next-line max-len
-    // 1. Set currentPocketIdx = 1 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-    // eslint-disable-next-line max-len
-    // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-    // eslint-disable-next-line max-len
-    // 2. Set currentPocketIdx = 1 -> endX = 2 -> startX = 3, check that carouselElement.current.children[currentPocketIdx - 1].scrollIntoView
-    // eslint-disable-next-line max-len
-    // === jest.fn was called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-    // eslint-disable-next-line max-len
-    // 3. Set currentPocketIdx = 2 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-    // eslint-disable-next-line max-len
-    // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
-
-    // eslint-disable-next-line max-len
-    // 4. Set currentPocketIdx = 0 -> endX = 2 -> startX = 1, check that carouselElement.current.children[currentPocketIdx + 1].scrollIntoView
-    // eslint-disable-next-line max-len
-    // === jest.fn WAS NOT called with {block: "start", inline: "nearest", behavior: "smooth"}. And test Dots via snapshot tests.
 });
