ec828bce5f9367ad3384b36e59fb27ff0cd3476b
eshekak <olenev.biz@gmail.com>
2020-09-08 09:52:55 +0300

    feat/connect-to-store: Minor

--- src/components/carousel/__tests__/carousel-factory.test.js
+++ src/components/carousel/__tests__/carousel-factory.test.js
@@ -1,11 +1,10 @@
-import * as React from "react";
+import React from "react";
 import renderer from "react-test-renderer";
 
 import CarouselFactory from "../carousel-factory.js";
 
 describe("<CarouselFactory />", () => {
     it("is rendered correctly", () => {
-        React.useRef = jest.fn().mockReturnValue({current: {children: [{}, {}, {scrollIntoView: () => ({})}]}});
         expect(renderer
             .create(<CarouselFactory />).toJSON())
             .toMatchSnapshot();
