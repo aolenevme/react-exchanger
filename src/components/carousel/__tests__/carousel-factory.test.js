bbd0b2587790ee0194de46492e9ec6a6ca9bdd01
eshekak <olenev.biz@gmail.com>
2020-09-08 11:35:52 +0300

    feat/connect-to-store: More snapshots test for the carousel factory

--- src/components/carousel/__tests__/carousel-factory.test.js
+++ src/components/carousel/__tests__/carousel-factory.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
+import * as store from "../../../store/store.js";
+import currencies from "../../../lib/consts/currencies/currencies.js";
 import CarouselFactory from "../carousel-factory.js";
 
 describe("<CarouselFactory />", () => {
@@ -11,6 +13,23 @@ describe("<CarouselFactory />", () => {
 
         expect(renderer
             .create(<CarouselFactory areTargetPockets/>).toJSON())
+            .toMatchSnapshot();
+    });
+
+    it("is rendered correctly with mocked store", () => {
+        store.default = {
+            ...store.default,
+            ...{
+                selectedCurrency: currencies.USD.abbreviation,
+                targetCurrency: currencies.USD.abbreviation
+            }
+        };
+        expect(renderer
+            .create(<CarouselFactory areTargetPockets/>).toJSON())
+            .toMatchSnapshot();
+
+        expect(renderer
+            .create(<CarouselFactory />).toJSON())
             .toMatchSnapshot();
     });
 });
