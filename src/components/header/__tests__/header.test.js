d498d15b188e162cf0e82df440b455cc1a905b9d
eshekak <olenev.biz@gmail.com>
2020-09-08 20:41:40 +0300

    feat/connect-to-store: Tests for the header

--- src/components/header/__tests__/header.test.js
+++ src/components/header/__tests__/header.test.js
@@ -5,6 +5,8 @@ import Header from "../header.js";
 import * as registry from "../../../lib/state-management/registry.js";
 import * as store from "../../../store/store.js";
 import currencies from "../../../lib/consts/currencies/currencies.js";
+import MUTATE_STORE from "../../../events/mutate-store.js";
+import checkStoreMutations from "../../../lib/tests/check-store-mutations.js";
 
 describe("<Header />", () => {
     beforeEach(() => {
@@ -62,6 +64,31 @@ describe("<Header />", () => {
                 .toJSON().children[0].props.onClick();
 
             expect(registry.dispatch).toHaveBeenCalled();
+        });
+
+        it("transfer money on Exchange and resets exchange amount", () => {
+            store.default = {
+                exchangeAmount: 10,
+                selectedCurrency: currencies.USD.abbreviation,
+                targetCurrency: currencies.EUR.abbreviation,
+                rates: {
+                    target: 0.9
+                },
+                balances: {
+                    [currencies.USD.abbreviation]: 50,
+                    [currencies.EUR.abbreviation]: 40
+                }
+            };
+
+            renderer
+                .create(<Header />)
+                // eslint-disable-next-line no-magic-numbers
+                .toJSON().children[2].props.onClick();
+
+            checkStoreMutations(0, MUTATE_STORE, [["balances", currencies.USD.abbreviation], "40.00"]);
+            checkStoreMutations(1, MUTATE_STORE, [["balances", currencies.EUR.abbreviation], "49.00"]);
+            // eslint-disable-next-line no-magic-numbers
+            checkStoreMutations(2, MUTATE_STORE, [["exchangeAmount"], ""]);
         });
     });
 });
